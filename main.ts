418140cda44071044465d7b0b5a36255a5a25176
PhilippAusHeidelberg <110686148+PhilippAusHeidelberg@users.noreply.github.com>
2022-08-30 08:25:55 +0200

    mit Variablen

--- main.ts
+++ main.ts
@@ -1,15 +1,13 @@
+let erste_Zahl = 0
+let zweite_Zahl = 0
 input.onButtonPressed(Button.A, function () {
-    basic.showNumber(randint(0, 10))
+    erste_Zahl = randint(0, 10)
+    basic.showNumber(erste_Zahl)
     basic.showString("+")
-    basic.showNumber(randint(0, 10))
+    zweite_Zahl = randint(0, 10)
+    basic.showNumber(zweite_Zahl)
     basic.showString("=")
 })
 input.onButtonPressed(Button.B, function () {
-    basic.showLeds(`
-        . . . . .
-        . . . . .
-        . . . . .
-        . . . . .
-        . . . . .
-        `)
+    basic.showNumber(erste_Zahl + zweite_Zahl)
 })
